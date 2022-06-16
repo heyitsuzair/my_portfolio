b48eac57f1b37a3fdc21ee8557e8fc0f301ea102
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-06-17 03:54:11 +0500

    No Edit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var typed = new Typed(".typing", {
+    strings: [
+      "Web Developer",
+      "Web Designer",
+      "Freelancer",
+      "WordPress Expert",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
---
+++ js/style-switcher.js
@@ -0,0 +1,37 @@
+// Toggle Style Switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggle");
+$(styleSwitcherToggle).on("click", function () {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+// Hide Style Switcher
+$(window).scroll(function () {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+//   Theme Colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+  alternateStyles.forEach(function (style) {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+// Theme Light And Dark Mode
+const dayNight = document.querySelector(".day-night");
+$(dayNight).click(function () {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+$(window).on("load", function () {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
