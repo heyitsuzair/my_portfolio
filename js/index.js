1a5b87b77c71474b86b7dd844432bf6ab182e895
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-06-17 05:23:41 +0500

    Added AOS Using Waypoints On Progress Bars

--- js/index.js
+++ js/index.js
@@ -10,4 +10,18 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
+  var waypoint = new Waypoint({
+    element: document.getElementById("waypoint-elem"),
+    handler: function (direction) {
+      let pBar = document.querySelectorAll(".progress-in");
+      pBar[0].setAttribute("style", "width: 90%;transition:1.3s all");
+      pBar[1].setAttribute("style", "width: 90%;transition:1.3s all");
+      pBar[2].setAttribute("style", "width: 75%;transition:1.3s all");
+      pBar[3].setAttribute("style", "width: 85%;transition:1.3s all");
+      pBar[4].setAttribute("style", "width: 85%;transition:1.3s all");
+      pBar[5].setAttribute("style", "width: 90%;transition:1.3s all");
+      pBar[6].setAttribute("style", "width: 90%;transition:1.3s all");
+    },
+    offset: "85%",
+  });
 });
