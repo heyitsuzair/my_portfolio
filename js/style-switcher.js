84ce86b38a472ac495cb98f97b0d759a96d70406
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-02 17:26:17 +0500

    Converted To OOp

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,37 +1,65 @@
-// Toggle Style Switcher
+// Variables
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggle");
-$(styleSwitcherToggle).on("click", function () {
-  document.querySelector(".style-switcher").classList.toggle("open");
-});
-// Hide Style Switcher
-$(window).scroll(function () {
-  if (document.querySelector(".style-switcher").classList.contains("open")) {
-    document.querySelector(".style-switcher").classList.remove("open");
-  }
-});
-//   Theme Colors
 const alternateStyles = document.querySelectorAll(".alternate-style");
-
-function setActiveStyle(color) {
-  alternateStyles.forEach(function (style) {
-    if (color === style.getAttribute("title")) {
-      style.removeAttribute("disabled");
-    } else {
-      style.setAttribute("disabled", "true");
-    }
-  });
-}
-// Theme Light And Dark Mode
 const dayNight = document.querySelector(".day-night");
-$(dayNight).click(function () {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
-  document.body.classList.toggle("dark");
-});
-$(window).on("load", function () {
-  if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun");
-  } else {
-    dayNight.querySelector("i").classList.add("fa-moon");
+
+// Style Switcher
+class styleSwitcher {
+  // hide on scroll
+  closeOnScroll() {
+    $(window).scroll(function () {
+      if (
+        document.querySelector(".style-switcher").classList.contains("open")
+      ) {
+        document.querySelector(".style-switcher").classList.remove("open");
+      }
+    });
   }
-});
+  openStyleSwitcher() {
+    $(styleSwitcherToggle).on("click", function () {
+      document.querySelector(".style-switcher").classList.toggle("open");
+    });
+  }
+}
+switcherObj = new styleSwitcher();
+switcherObj.closeOnScroll();
+switcherObj.openStyleSwitcher();
+//   Theme Colors
+class themeColors {
+  setActiveStyle(color) {
+    alternateStyles.forEach(function (style) {
+      if (color === style.getAttribute("title")) {
+        style.removeAttribute("disabled");
+      } else {
+        style.setAttribute("disabled", "true");
+      }
+    });
+  }
+}
+themeColors = new themeColors();
+themeColors.setActiveStyle();
+// Theme Light And Dark Mode
+class themeMode {
+  constructor() {
+    this.dayNight = dayNight;
+  }
+  setDayNight() {
+    $(dayNight).click(function () {
+      dayNight.querySelector("i").classList.toggle("fa-sun");
+      dayNight.querySelector("i").classList.toggle("fa-moon");
+      document.body.classList.toggle("dark");
+    });
+  }
+  setDefaultMode() {
+    $(window).on("load", function () {
+      if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+      } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+      }
+    });
+  }
+}
+themeMode = new themeMode();
+themeMode.setDayNight();
+themeMode.setDefaultMode();
